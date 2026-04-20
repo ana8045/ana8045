3f01535eca8f22c1164e8a9795e94f71483ef34b
ana8045 <ananyatiwari995@gmail.com>
2026-04-20 06:02:04 +0530

    changes

--- Terraform/provider.tf
+++ Terraform/provider.tf
@@ -1,21 +1,13 @@
 terraform {
-<<<<<<< HEAD
   required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
-=======
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
       version = "~> 6.0"
->>>>>>> 0106939376f3c39e316257bd52e210a3ab28c6c5
     }
   }
 }
 
-<<<<<<< HEAD
 provider "aws" {
   region = var.aws_region
 
@@ -26,14 +18,9 @@ provider "aws" {
       ManagedBy   = "Terraform"
     }
   }
-=======
+}
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-# Create a VPC
-resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
->>>>>>> 0106939376f3c39e316257bd52e210a3ab28c6c5
-}
