b6af2edd1e4122477dbeeeba76520754f148250e
Ananya Tiwari <105954941+ana8045@users.noreply.github.com>
2026-04-21 07:03:27 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,30 +18,13 @@ module "eks" {
     resources        = ["secrets"]
   }
 
-  eks_managed_node_groups = {
-    default = {
-      instance_types = [var.node_instance_type]
-      desired_size   = var.node_desired_size
-      min_size       = 1
-      max_size       = 4
-
-      tags = {
-        NodeGroup = "default"
-      }
-    }
-  }
 
   tags = {
     Name = "demo-eks-cluster"
   }
 }
 
-# KMS key for cluster encryption
-resource "aws_kms_key" "eks" {
-  description             = "KMS key for EKS cluster encryption"
-  deletion_window_in_days = 10
-  enable_key_rotation     = true
-
+         
   tags = {
     Name = "demo-eks-key"
   }
