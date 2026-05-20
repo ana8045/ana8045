9380300db807583e9797e1036cb8aae002a7a32c
Ananya Tiwari <105954941+ana8045@users.noreply.github.com>
2026-05-20 16:38:49 +0530

    Refactor S3 bucket resource configuration
    Updated S3 bucket resource to use 'each.key' for bucket name and added 'Environment' tag.

--- terraform/S3.tf
+++ terraform/S3.tf
@@ -1,8 +1,14 @@
 resource "aws_s3_bucket" "My_learning" {
-    bucket = var.S3_bucket_name
-    for_each = toset(["${var.S3_bucket_name}-1", "${var.S3_bucket_name}-2", "${var.S3_bucket_name}-3"])
-    tags = {
-      Name        = var.S3_bucket_name
-    }
-  
+  for_each = toset([
+    "${var.S3_bucket_name}-1",
+    "${var.S3_bucket_name}-2",
+    "${var.S3_bucket_name}-3"
+  ])
+
+  bucket = each.key
+
+  tags = {
+    Name        = each.key
+    Environment = var.Environment
+  }
 }
