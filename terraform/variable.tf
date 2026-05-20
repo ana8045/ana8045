85772f0f145f53bd399e2ff2d8e69af34bce7a4a
Ananya Tiwari <105954941+ana8045@users.noreply.github.com>
2026-05-20 16:01:18 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -6,7 +6,7 @@ variable "region" {
 variable "S3_bucket_name" {
   description = "The name of the S3 bucket to create."
   type        = string
-  default     = "my-learning-bucket"
+  default     = "my-learning-bucket5646"
 }
 variable "Environment" {
   description = "The environment for the resources."
