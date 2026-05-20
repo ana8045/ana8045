74c1e609b837fcde025c5f4931e8f924c0b08fcb
Ananya Tiwari <105954941+ana8045@users.noreply.github.com>
2026-05-20 15:38:08 +0530

    Change for_each from tolist to toset

--- terraform/S3.tf
+++ terraform/S3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket" "My_learning" {
     bucket = var.S3_bucket_name
-    for_each = tolist(["${var.S3_bucket_name}-1", "${var.S3_bucket_name}-2", "${var.S3_bucket_name}-3"])
+    for_each = toset(["${var.S3_bucket_name}-1", "${var.S3_bucket_name}-2", "${var.S3_bucket_name}-3"])
     tags = {
       Name        = var.S3_bucket_name
       Environment = var.Environment
