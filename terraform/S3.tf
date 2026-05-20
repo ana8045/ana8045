de5b1c22684337054aa2269d1c03c50789bdc87f
Ananya Tiwari <105954941+ana8045@users.noreply.github.com>
2026-05-20 15:54:09 +0530

    Remove Environment tag from S3 bucket
    Removed the Environment tag value from S3 bucket resource.

--- terraform/S3.tf
+++ terraform/S3.tf
@@ -3,7 +3,6 @@ resource "aws_s3_bucket" "My_learning" {
     for_each = toset(["${var.S3_bucket_name}-1", "${var.S3_bucket_name}-2", "${var.S3_bucket_name}-3"])
     tags = {
       Name        = var.S3_bucket_name
-      Environment = us-east-1
     }
   
 }
