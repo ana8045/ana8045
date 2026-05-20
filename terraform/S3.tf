8d8fe43b3790b6a5ee9e292792ccbccd5291fc93
Ananya Tiwari <105954941+ana8045@users.noreply.github.com>
2026-05-20 15:52:59 +0530

    Update Environment tag value in S3.tf
    Changed the Environment tag value to 'us-east-1'.

--- terraform/S3.tf
+++ terraform/S3.tf
@@ -3,7 +3,7 @@ resource "aws_s3_bucket" "My_learning" {
     for_each = toset(["${var.S3_bucket_name}-1", "${var.S3_bucket_name}-2", "${var.S3_bucket_name}-3"])
     tags = {
       Name        = var.S3_bucket_name
-      Environment = var.Environment
+      Environment = us-east-1
     }
   
 }
